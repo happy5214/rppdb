1bf6baf5b812714d7eb1fcd20f5569a87815a4b3
Alexander Jones <happy5214@gmail.com>
2017-08-12 17:53:21 -0500

    Remove Acme test bundle

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once("/etc/php5/globals.php");
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
@@ -13,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array($happy5214IPAddress, $laptopIPAddress, '127.0.0.1', 'fe80::1', '::1'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
